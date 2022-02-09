34bfea01a8ea8e5b7e24e918f46108a0c3cfcc81
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-02-09 08:35:54 -0600

    changing remaining file names

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+
+
 window.onscroll = function() {scrollFunction()};
+
+document.getElementById("jumbotron").style.height = window.innerHeight;
+
  
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
